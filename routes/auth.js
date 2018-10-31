17dcd8bfa7516b294a92d0d693d1660519efe49e
Beaule <frederic.lebeau.mobile@gmail.com>
2018-10-31 22:22:07 +0100

    remove console.log

--- routes/auth.js
+++ routes/auth.js
@@ -27,7 +27,6 @@ router.get('/login', passport.authenticate('auth0', {
 // Perform the final stage of authentication and redirect to previously requested URL or '/user'
 router.get('/callback', function (req, res, next) {
   passport.authenticate('auth0', function (err, user, info) {
-    console.log(info);
     if (info=="unauthorized") {  return res.redirect('/activateEmail'); }
     if (err) {  return next(err); }
     if (!user) { return res.redirect('/'); }
