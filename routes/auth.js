431a4ef7f8b52cf9b0cc704126628463e74c6914
Beaule <frederic.lebeau.mobile@gmail.com>
2018-10-31 21:56:30 +0100

    debug email verification

--- routes/auth.js
+++ routes/auth.js
@@ -27,8 +27,10 @@ router.get('/login', passport.authenticate('auth0', {
 // Perform the final stage of authentication and redirect to previously requested URL or '/user'
 router.get('/callback', function (req, res, next) {
   passport.authenticate('auth0', function (err, user, info) {
-    if (err) { return next(err); }
-    if (!user) { return res.redirect('/login'); }
+    console.log(info);
+    if (info=="unauthorized") {  return res.redirect('/activateEmail'); }
+    if (err) {  return next(err); }
+    if (!user) { return res.redirect('/'); }
     req.logIn(user, function (err) {
       if (err) { return next(err); }
       const returnTo = req.session.returnTo;
@@ -38,10 +40,33 @@ router.get('/callback', function (req, res, next) {
   })(req, res, next);
 });
 
+// email needs to be activatedxs
+router.get('/activateEmail', function (req, res, next) {
+  renderActivateEmail(req,res);
+});
+
 // Perform session logout and redirect to homepage
 router.get('/logout', (req, res) => {
   req.logout();
   res.redirect('/');
 });
+
+
+/***********************************
+ * rendering functions
+ ************************************/
+
+/**
+ * render activateEmail
+ * @param {req} request
+ * @param {res} response
+ */
+function renderActivateEmail(req,res){
+  res.render('activateEmail', {
+    title: 'Activate email',
+    layout: 'singlePage'
+  });
+}
+
 
 module.exports = router;
