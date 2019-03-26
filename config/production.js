d6050c8eccbfbfb46f7a7adee365bb367d461cb6
Beaule <frederic.lebeau.mobile@gmail.com>
2019-03-26 21:42:48 +0100

    fix configs

--- config/production.js
+++ config/production.js
@@ -3,6 +3,10 @@
 
 module.exports = {
   env: 'production',
-  db: process.env.MONGOHQ_URL || process.env.MONGODB_URI,
-  port: process.env.PORT || 4000,
+  port: process.env.PORT || 3000,
+  secret: process.env.SESSION_SECRET,
+  domain: process.env.AUTH0_DOMAIN,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  callbackURL:process.env.AUTH0_CALLBACK_URL 
 };
--- config/test.js
+++ config/test.js
@@ -2,6 +2,10 @@
 
 module.exports = {
   env: 'test',
-  db: 'mongodb://localhost/TododbTest',
-  port: process.env.PORT || 4100,
+  port: process.env.PORT || 3000,
+  secret: process.env.SESSION_SECRET,
+  domain: process.env.AUTH0_DOMAIN,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  callbackURL:process.env.AUTH0_CALLBACK_URL 
 };
